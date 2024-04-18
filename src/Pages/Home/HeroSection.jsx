2e0794b29b61ace29cd0d4efa054767405cfafd1
Roseuwizeye123 <marieroseuwizeyimana440@gmail.com>
2024-04-18 16:12:51 -0700

    navbar and about me

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -3,9 +3,9 @@ export default function HeroSection() {
     <section id="heroSection" className="hero--section">
       <div className="hero--section--content--box">
         <div className="hero--section--content">
-          <p className="section--title">Hey, I'm John</p>
+          <p className="section--title">Hey, I'm Marie</p>
           <h1 className="hero--section--title">
-            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <span className="hero--section-title--color">Front-end</span>{" "}
             <br />
             Developer
           </h1>
@@ -17,7 +17,7 @@ export default function HeroSection() {
         <button className="btn btn-primary">Get In Touch</button>
       </div>
       <div className="hero--section--img">
-        <img src="./img/hero_img.png" alt="Hero Section" />
+        <img src="./img/log.png" alt="Hero Section" />
       </div>
     </section>
   );
